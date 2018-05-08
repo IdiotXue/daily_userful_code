4e94622c35d51d230b7c111f2f39d873289fd441
zero <xue.zl@qq.com>
2018-05-08 23:36:33 +0800

    init move all useful code from offerExam

---
+++ README.md
@@ -0,0 +1,12 @@
+# 日常codeing的存档，记录一些有用的小实验
+1. 在useful_code目录中复制一个cpp文件到src目录：cp useful_code/xx.cpp src/main.cpp
+2. 执行 ./build即可运行，缺bin和obj目录就创建一下
+
+# 摘要
+- IPC_Dinning_Philosophers: 哲学家进餐问题，多线程下的解法
+- istringstream_usage: istringstream和getline简化文件读写
+- judge_byte_order: 判断系统字节序
+- lseek_write: lseek产生文件空洞的实验
+- postfixEpr_to_EprTree: 后缀表达式 转 表达式树
+- replace_char: 排列+大数问题的字符串替换
+- several_sort: 实现多种排序，包括插入、谢尔、自己实现堆、STL堆、归并、三数中值的快排、间接排序、拓扑排序
---
+++ useful_code/replace_char.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+/**
+ * 题目：输入一个字符串和两个字符，输出将给定字符替换为另一个的所有可能结果
+ * 如输入 abb b d
+ * 输出：abb,abd,adb,add
+ */
+bool IsOver(vector<int> &num)
+{
+    bool bIsOver = false;
+    int nCarryBit = 0, nBitSum = 0;
+    for (int i = num.size() - 1; i >= 0; --i)
+    {
+        nBitSum = num[i] + nCarryBit;
+        if (i == num.size() - 1)
+            ++nBitSum;
+        if (nBitSum >= 2) // 进位
+        {
+            if (i == 0) //溢出
+                bIsOver = true;
+            else
+            {
+                nCarryBit = 1;
+                num[i] = nBitSum - 2;
+            }
+        }
+        else
+        {
+            num[i] += 1;
+            break;
+        }
+    }
+    return bIsOver;
+}
+
+int main(int argc, char const *argv[])
+{
+    string str;
+    char oc, nc;
+
+    while (cin >> str >> oc >> nc)
+    {
+        vector<int> vnIndex;
+        int diff = 'a' - 'A';
+        for (size_t i = 0; i < str.size(); ++i)
+        {
+            if (str[i] == oc || str[i] == (oc - diff) || str[i] == (oc + diff))
+                vnIndex.push_back(i);
+        }
+        vector<int> binary(vnIndex.size(), 0);
+        cout << str << ',';
+        int times = pow(2, vnIndex.size());
+        for (int i = 1; i < times; ++i)
+        {
+            IsOver(binary);
+            string temp = str;
+            for (size_t i = 0; i < vnIndex.size(); ++i)
+            {
+                if (binary[i])
+                    temp[vnIndex[i]] = nc;
+            }
+            cout << temp;
+            if (i == times - 1)
+                break;
+            cout << ',';
+        }
+        cout << endl;
+    }
+}
